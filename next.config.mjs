9c88624c155c0ce67038c15bf391e8c7beba5cc8
warmymin <02forest@naver.com>
2025-07-24 08:41:21 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
     eslint: {
       ignoreDuringBuilds: true,
     },
   };
   
   export default nextConfig;
-=======
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-export default nextConfig;
->>>>>>> 2c230864dc32e186b7f7271db01a8cb7491f68bf
